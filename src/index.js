ca29e634828a72cf32d965bec5cd95beba895952
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2025-03-21 03:37:42 -0300

    C:/Program Files/Git/api para as rotas

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(router);
+app.use("/api", router);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
