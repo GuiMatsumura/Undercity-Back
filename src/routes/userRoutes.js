d989210a8cfaab31b9d19b4490ad43a036811ab3
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2025-03-21 16:49:33 -0300

    adicionando o validateUser a rota

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import userController from "../controllers/userController.js";
+import validateUser from "../middlewares/validateUser.js";
 
 const router = Router();
 
-router.post("/register", userController.registerUser);
+router.post("/register", validateUser, userController.registerUser);
 
 export default router;
