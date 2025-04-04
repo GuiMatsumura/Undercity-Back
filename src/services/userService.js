96caf659e579bac11fb6816312ad9ab4cc86b957
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2025-04-04 17:10:41 -0300

    service para logar usuario

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,8 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import userRepository from "../repositories/userRepository.js";
+
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const registerUser = async ({ name, email, password, role }) => {
   const existingUser = await userRepository.findUserByEmail(email);
@@ -17,4 +20,16 @@ const registerUser = async ({ name, email, password, role }) => {
   });
 };
 
-export default { registerUser };
+const login = async ({ email, password }) => {
+  const user = await userRepository.findUserByEmail(email);
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return { success: false, message: "Credenciais inválidas" };
+  }
+
+  const token = jwt.sign({ id: user.id, email: email }, SECRET_KEY, {
+    expiresIn: "7d",
+  });
+  return { success: true, token };
+};
+
+export default { registerUser, login };
