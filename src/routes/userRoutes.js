672ec4dda4e31a9aaf8c9a827ad163bc9e8e5cf7
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2025-04-04 17:11:55 -0300

    rota de login com middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import userController from "../controllers/userController.js";
 import validateUser from "../middlewares/validateUserRegister.js";
+import validateLogin from "../middlewares/validateLogin.js";
 
 const router = Router();
 
 router.post("/register", validateUser, userController.registerUser);
+router.post("/login", validateLogin, userController.login);
 
 export default router;
